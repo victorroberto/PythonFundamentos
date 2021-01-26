3795ae5f7aab5ab1720062493ca28b495914cb80
Victor Roberto da Silva Santos <victorroberto@outlook.com>
2021-01-26 13:36:44 -0300

    Excluido respostas

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -4,29 +4,4 @@
 # A solução será apresentada no próximo capítulo!
 # Assista o vídeo com a execução do programa!
 
-print("\n******************* Python Calculator *******************")
-
-print('Selecione o número da operação desejada: \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão')
-
-operacao = input('Digite a sua opção (1/2/3/4):')
-primeiroNum = float(input('Digite o primeiro número:'))
-segundoNum = float(input('Digite o segundo número:'))
-
-def imprime(primeiroNum, segundoNum, operador, resultado):
-    print(str(primeiroNum) + operador + str(segundoNum) + ' = ' + str(resultado))
-    
-if operacao == '1':
-    resultado = lambda x, y: x + y
-    imprime(primeiroNum, segundoNum, ' + ' , resultado(primeiroNum, segundoNum))
-elif operacao == '2':
-    resultado = lambda x, y: x - y
-    imprime(primeiroNum, segundoNum, ' - ' , resultado(primeiroNum, segundoNum))
-elif operacao == '3':
-    resultado = lambda x, y: x * y
-    imprime(primeiroNum, segundoNum, ' * ' , resultado(primeiroNum, segundoNum))
-elif operacao == '4':
-    resultado = lambda x, y: x / y
-    imprime(primeiroNum, segundoNum, ' / ' , resultado(primeiroNum, segundoNum))
-else:
-    print('Opção Invalida') 
 
--- Cap04/Lab02/calculadora_v2.py
+++ Cap04/Lab02/calculadora_v2.py
@@ -2,51 +2,29 @@
 
 print("\n******************* Python Calculator *******************")
 
-def add(x, y):
-	return x + y
+print('Selecione o número da operação desejada: \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão')
 
-def subtract(x, y):
-	return x - y
+operacao = input('Digite a sua opção (1/2/3/4):')
+primeiroNum = float(input('Digite o primeiro número:'))
+segundoNum = float(input('Digite o segundo número:'))
 
-def multiply(x, y):
-	return x * y
-
-def divide(x, y):
-	return x / y
-
-print("\nSelecione o número da operação desejada: \n")
-print("1 - Soma")
-print("2 - Subtração")
-print("3 - Multiplicação")
-print("4 - Divisão")
-
-escolha = input("\nDigite sua opção (1/2/3/4): ")
-
-num1 = int(input("\nDigite o primeiro número: "))
-num2 = int(input("\nDigite o segundo número: "))
-
-if escolha == '1':
-	print("\n")
-	print(num1, "+", num2, "=", add(num1, num2))
-	print("\n")
-
-elif escolha == '2':
-	print("\n")
-	print(num1, "-", num2, "=", subtract(num1, num2))
-	print("\n")
-
-elif escolha == '3':
-	print("\n")
-	print(num1, "*", num2, "=", multiply(num1, num2))
-	print("\n")
-
-elif escolha == '4':
-	print("\n")
-	print(num1, "/", num2, "=", divide(num1, num2))
-	print("\n")
-
+def imprime(primeiroNum, segundoNum, operador, resultado):
+    print(str(primeiroNum) + operador + str(segundoNum) + ' = ' + str(resultado))
+    
+if operacao == '1':
+    resultado = lambda x, y: x + y
+    imprime(primeiroNum, segundoNum, ' + ' , resultado(primeiroNum, segundoNum))
+elif operacao == '2':
+    resultado = lambda x, y: x - y
+    imprime(primeiroNum, segundoNum, ' - ' , resultado(primeiroNum, segundoNum))
+elif operacao == '3':
+    resultado = lambda x, y: x * y
+    imprime(primeiroNum, segundoNum, ' * ' , resultado(primeiroNum, segundoNum))
+elif operacao == '4':
+    resultado = lambda x, y: x / y
+    imprime(primeiroNum, segundoNum, ' / ' , resultado(primeiroNum, segundoNum))
 else:
-	print("\nOpção Inválida!")
+    print('Opção Invalida') 
 
 	
 	
